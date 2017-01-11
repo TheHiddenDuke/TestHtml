019818245b9589a90aa338e72f46593f6f9a70ca
TheHiddenDuke <jarand_ss@hotmail.com>
2017-01-11 13:28:46 +0100

    Login screen 11.01.2017

--- php/Main.php
+++ php/Main.php
@@ -5,3 +5,9 @@
  * Date: 10/01/2017
  * Time: 12:51
  */
+
+if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+    echo 'We don\'t have mysqli!!!';
+} else {
+    echo 'Phew we have it!';
+}
--- php/checklogin.php
+++ php/checklogin.php
@@ -10,13 +10,12 @@
 ob_start();
 $host="127.0.0.1"; // Host name
 $username="root"; // Mysql username
-$password="MilitaryDock1"; // Mysql password
-$db_name="Login"; // Database name
-$tbl_name="members"; // Table name
+$password="heihei"; // Mysql password
+$db_name="innlogging"; // Database name
+$tbl_name="users"; // Table name
 
 // Connect to server and select databse.
-$mysqli = new mysqli("$host", "$username", "$password")or die("cannot connect");
-mysqli::select_db("$db_name")or die("cannot select DB");
+$mysqli = new mysqli("$host", "$username", "$password", "$db_name")or die("cannot connect");
 
 // Define $myusername and $mypassword
 $myusername=$_POST['myusername'];
@@ -25,20 +24,20 @@ $mypassword=$_POST['mypassword'];
 // To protect MySQL injection (more detail about MySQL injection)
 $myusername = stripslashes($myusername);
 $mypassword = stripslashes($mypassword);
-$myusername = mysqli::real_escape_string($myusername);
-$mypassword = mysqli::real_escape_string($mypassword);
+$myusername = $mysqli->real_escape_string($myusername);
+$mypassword = $mysqli->real_escape_string($mypassword);
 $sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
-$result=mysqli::query($sql);
+$result=$mysqli->query($sql);
 
 // Mysql_num_row is counting table row
-$count=mysql_num_rows($result);
+$count=mysqli_num_rows($result);
 
 // If result matched $myusername and $mypassword, table row must be 1 row
 if($count==1){
 
 // Register $myusername, $mypassword and redirect to file "login_success.php"
-    mysqli::session_register("myusername");
-    mysqli::session_register("mypassword");
+    $mysqli->$_SESSION("myusername");
+    $mysqli->$_SESSION("mypassword");
     header("location:login_success.php");
 }
 else {
