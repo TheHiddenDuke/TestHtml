b62aafc5cf9cdf9dd352d1d2679d8c59bfa123cb
TheHiddenDuke <jarand_ss@hotmail.com>
2017-01-11 11:58:01 +0100

    Login screen 11.01.2017

---
+++ php/Logout.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jarand
+ * Date: 11/01/2017
+ * Time: 11:03
+ */
+
+session_start();
+session_destroy();
+?>
---
+++ php/loginsuccess.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jarand
+ * Date: 11/01/2017
+ * Time: 11:00
+ */
+session_start();
+if(!session_is_registered(myusername)){
+    header("location:main_login.php");
+}
+?>
+
+<html>
+<body>
+Login Successful
+</body>
+</html>
