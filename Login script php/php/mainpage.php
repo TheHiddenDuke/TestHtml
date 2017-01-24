a2f38567f22ae42df9c779369d62f9a94c29e389
TheHiddenDuke <jarand_ss@hotmail.com>
2017-01-24 11:27:15 +0100

    Checkbox fixed start of buypage

--- Login script php/php/mainpage.php
+++ Login script php/php/mainpage.php
@@ -6,39 +6,37 @@
  * Time: 10:57
  */
 ?>
-<link rel="stylesheet" href="../css/style.css">
-
-
+    <link rel="stylesheet" href="../css/style.css">
 <?php
 session_start();
 
 if (isset($_SESSION['isloggedin']) && $_SESSION['isloggedin'] == true) {
     echo "Welcome to the member's area, " . $_SESSION['username'] . "!";
 ?>
+    <!-- Login box-->
     <html>
-<body>
-<table width="300" id="rightWrapper" border="1"  cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-    <div id="rightTop">
-    <tr>
-            <td>
-                <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-                    <tr>
-                        <td colspan="3" align="center"><font size="6"> You are logged in </font></td>
-                    </tr>
-                    <form name="form1" method="post" action="logout.php">
-                    <tr>
-                        <td width="78">Logout?</td>
-                        <td width="6">:</td>
-                        <td width="294"><input type="submit" name="Logout" value="Logout"></td>
-                    </tr>
-                    </form>
-                </table>
-            </td>
-    </tr>
-    </div>
-</table>
-</body>
-</html>
+    <body>
+    <table width="300" align="right" border="1"  cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+            <tr>
+                <td>
+                    <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+                        <tr>
+                            <td colspan="3" align="center"><font size="6"> You are logged in </font></td>
+                        </tr>
+                        <form name="form1" method="post" action="logout.php">
+                            <tr>
+                                <td width="78">Logout?</td>
+                                <td width="6">:</td>
+                                <td width="294"><input type="submit" name="Logout" value="Logout"></td>
+                            </tr>
+                        </form>
+                    </table>
+                </td>
+            </tr>
+        </div>
+    </table>
+    </body>
+    </html>
 <?php
 } else {
     echo "Please log in first to see this page.";
@@ -83,42 +81,30 @@ if (isset($_SESSION['isloggedin']) && $_SESSION['isloggedin'] == true) {
 $mysqli = new mysqli("localhost", "root", "heihei", "innlogging")or die("cannot connect");
 $result = $mysqli -> query("SELECT itemname FROM items");
 
-    //checkbox list
 ?>
 
+    <!--Checkbox list-->
     <form method="post" id="shoppinglist" action="buypage.php">
-
         <table id="rightWrapper" width="300" cellpadding="0" cellspacing="1" border="1">
-<div id="rightBottom">
-            <tr><td>
-    <strong>Shopping:</strong><br>
-
-
-
-
-
+            <div id="rightBottom">
+                <tr>
+                    <td>
+                        <strong>Shopping:</strong><br>
 <?php
-
-
-
 while( $name = mysqli_fetch_assoc($result)):
 ?>
 
-
-        <input type="checkbox" name="itemname[]" value=" <?php echo $name['itemname']; ?> "/>
-         <?php echo $name['itemname']; ?><br>
-
+    <input type="checkbox" name="itemname[]" value=" <?php echo $name['itemname']; ?> "/>
+    <?php echo $name['itemname']; ?><br>
 
 <?php endwhile; ?>
-                <input type="submit" name="submit" value="checkout">
-        </td>
-</tr>
-</div>
+                        <input type="submit" name="submit" value="checkout">
+                    </td>
+                </tr>
+            </div>
         </table>
-
     </form>
+
 <?php
-
 //$_SESSION['isloggedin'] = false;
-
 ?>
